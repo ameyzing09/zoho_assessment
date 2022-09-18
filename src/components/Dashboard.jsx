f31ebc0d19d176fff48d10747c4fa31cf6cfb7a8
Amey Kode <55131226+ameyzing09@users.noreply.github.com>
2022-09-19 00:52:06 +0530

    Added dialog (#1)
    Co-authored-by: = <Amey Kode>

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,25 +1,51 @@
-import React, { useState } from "react";
-import Add from "./Add";
+import React, { useEffect, useState } from "react";
+
+import ModelRoot from "../services/modules/modals/modalRoot";
+import ModelService from "../services/modules/modals/modalService";
+import AddVehicleEntry from "./AddVehicleEntry";
+import AddTollEntry from "./AddTollEntry";
+
 import Header from "./Header";
 import List from "./List";
+import SearchBar from "./SearchBar";
 
 const Dashboard = () => {
+  useEffect(() => {
+    // get all list of all latest available vehicle list and set it to setVehicleList
+  });
+  const [tollList, setTollList] = useState();
   const [vehiclesList, setVehiclesList] = useState();
-  const [isToll, setIsToll] = useState(false);
-  const [isVehicle, setIsVehicle] = useState(false);
+  const [searchVehicleQuery, setSearchVehicleQuery] = useState("");
 
+  const openVehicleEntryForm = () => ModelService.open(AddVehicleEntry);
+  const openTollEntryForm = () => ModelService.open(AddTollEntry);
 
   return (
-    <div>
-      {!isToll && !isVehicle && (
-        <>
-          <Header  />
-          <List list={vehiclesList} />
-        </>
-      )}
-      {isToll && <Add setIsToll={setIsToll} />}
-      {isVehicle && <Add setIsVehicle={setIsVehicle} />}
-    </div>
+    <>
+      <div>
+        <Header />
+        <ModelRoot />
+        <div className='header-items'>
+          <SearchBar
+            setSearchVehicleQuery={setSearchVehicleQuery}
+            searchVehicleQuery={searchVehicleQuery}
+            list={vehiclesList}
+          />
+          <div className='header-buttons'>
+            <button className='btn-primary' onClick={openTollEntryForm}>
+              Add new toll
+            </button>
+            <button className='btn-primary' onClick={openVehicleEntryForm}>
+              Add vehicle entry
+            </button>
+            <button className='btn-primary'>View toll</button>
+          </div>
+        </div>
+      </div>
+      <div>
+        <List query={searchVehicleQuery} list={vehiclesList} />
+      </div>
+    </>
   );
 };
 
