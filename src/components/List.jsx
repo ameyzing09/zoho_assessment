df84ce9b62113b1e97b7ed7f1a24bb46a673b06e
Aishwarya Patre <aishwaryapatre1996@gmail.com>
2022-09-22 19:00:55 +0530

    list

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,13 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const List = ({ query ,list }) => { // list variable will be a object iterate using .reduce
-
-
-  const data = [
+const List = () => { // list variable will be a object iterate using .reduce
+   const data = [
     {
           vehicleType : "Jeep",
           VehicleNo : "TN24Aq4644",
-          datetime : "16-10-2019 ",15:30,
+          datetime : "16-10-2019 ,15:30",
           tollname:"Pune",
           tarrif : 60
   
@@ -44,9 +42,87 @@ const List = ({ query ,list }) => { // list variable will be a object iterate us
           tollname:"Pune",
           tarrif : 350
   
-      }
+      },
+      {
+        vehicleType : "Van",
+        VehicleNo :"TN24Aq4914",
+        datetime :  "09-08-2022" ,15:30,
+        tollname:"Nagpur",
+        tarrif : 90
+
+    },
+    {
+        vehicleType : "Heavy vehicle",
+        VehicleNo : "MH24LN3000",
+        datetime : "11-02-2022 ", 15:30,
+        tollname:"Pune",
+        tarrif : 350
+
+    },
+    {
+          
+      vehicleType : "car",
+      VehicleNo : "MH12gn8991",
+      datetime : "16-10-2022 " ,18:30,
+      tollname:"Pune",
+      tarrif : 60
+
+  },
+  {
+      vehicleType : "Van",
+      VehicleNo :"TN24AK6094",
+      datetime :  "12-08-2022" ,15:30,
+      tollname:"Nagpur",
+      tarrif : 150
+
+  },
+  {
+          
+    vehicleType : "Truck",
+    VehicleNo : "MH12cq2011",
+    datetime : "16-10-2022 " ,18:30,
+    tollname:"Pune",
+    tarrif : 250
+
+},
+{
+    vehicleType : "Van",
+    VehicleNo :"TN24Aq4644",
+    datetime :  "16-08-2022" ,15:30,
+    tollname:"Nagpur",
+    tarrif : 150
+
+},{
+          
+  vehicleType : "Heavy vehicle",
+  VehicleNo : "TN12AQ4042",
+  datetime : "16-10-2022 " ,18:30,
+  tollname:"Pune",
+  tarrif : 350
+
+},
+{
+  vehicleType : "Van",
+  VehicleNo :"TN24Aq4644",
+  datetime :  "16-08-2022" ,15:30,
+  tollname:"Nagpur",
+  tarrif : 150
+},
+
   ]
-  
+  // setDeleteRow([...deleterow, data])
+//   const handleRemoveSpecificRow = index => () => {
+//     let items = rows.filter(row => row.id != val.vehicleType);
+//     setRows(items);
+// };
+const [deleterow,setDeleteRow] = useState(data)
+
+let handleDelete=(e,index)=>{
+  const List = [...deleterow];
+  List.splice(index, 1);
+  setDeleteRow(List);
+}
+
   return (
     <div className="App">
     <table>
@@ -57,7 +133,7 @@ const List = ({ query ,list }) => { // list variable will be a object iterate us
         <th>TOLL NAME</th>
         <th>TARRIF</th>
       </tr>
-      {data.map((val, key) => {
+      {deleterow.map((val, key) => {
         return (
           <tr key={key}>
             <td>{val.vehicleType}</td>
@@ -65,9 +141,11 @@ const List = ({ query ,list }) => { // list variable will be a object iterate us
             <td>{val.datetime}</td>
             <td>{val.tollname}</td>
             <td>{val.tarrif}</td>
+            <td> <button type="button"  onClick={ (e) => handleDelete(e, key)} className = "btn btn-close">Delete</button></td>
           </tr>
         )
       })}
+     
     </table>
   </div>
   )
